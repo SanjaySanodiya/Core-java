6af16a861cdc05b0641d4b07406c22fc6794fa3c
sanja <sanja@LAPTOP-VCTN4LT8>
2022-06-07 12:53:59 +0530

    First commit

---
+++ AdvanceJava/src/StreamAPI/StreamApi3.java
@@ -0,0 +1,21 @@
+package StreamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamApi3 
+{
+	public static void main(String[] args) {
+		
+		List<String> namelist = new ArrayList<>();
+		namelist.add("INDIA");
+		namelist.add("RUSSIA");
+		namelist.add("AMERICA");
+		namelist.add("ENGLAND");
+		namelist.add("AUSTRALIA");
+		namelist.add("ZIMBABWE");
+		
+		Long count = namelist.stream().filter(s -> s.length() > 5).count();
+		System.out.println("Total name with more than 5 lenth is : "+count);  // The output is 5.
+	}
+}
